b018769fc461c1645d1d23615715a3be938fa66f
mandy <mandy1030136@gmail.com>
2025-02-20 23:44:35 +0800

    update source code

---
+++ assets/Scripts/bullet.ts
@@ -0,0 +1,72 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class bullet extends cc.Component {
+
+    private targetPosition: cc.Vec2 = cc.v2(0, 0);
+
+    private bulletManager = null;
+
+    public isTriggered = false; // I add this to make the bullet kill one enemy at a time.
+
+    // when created, the bullet need to be placed at correct position and play animation.
+    public init(node: cc.Node, targetPosition: cc.Vec2, targetDirection: string) {
+        this.setInitPos(node, targetDirection);
+        this.bulletMove()
+        this.targetPosition = targetPosition;
+    }
+
+    // this function is called when the bullet manager calls "get" API.
+    reuse(bulletManager) {
+        this.bulletManager = bulletManager;
+
+        this.isTriggered = false;
+    }
+
+    //this function sets the bullet's initial position when it is reused.
+    setInitPos(node: cc.Node, targetDirection: string) {
+        this.node.parent = node.parent; // don't mount under the player, otherwise it will change direction when player move
+        
+        this.node.setPosition(cc.v2(0, 0));
+        this.node.position = this.node.position.addSelf(node.position);
+    }
+
+    //make the bullet move from current position
+    bulletMove() {
+
+        let moveDir = cc.moveTo(0.2, this.targetPosition);
+
+        // move bullet to 500 far from current position in 0.8s
+
+        let finished = cc.callFunc(() => {
+            this.bulletManager.put(this.node);
+        });
+
+        // after playing animation, the bullet move 0.8s and destroy itself(put back to the bullet manager)
+        this.scheduleOnce(() => {
+            this.node.runAction(cc.sequence(moveDir, finished));
+        });
+    }
+
+    //detect collision with enemies
+    onBeginContact(contact, selt, other) {
+        if(other.tag == 1){
+            contact.disabled = true;
+            return;
+        }
+        this.node.stopAllActions();
+
+        this.unscheduleAllCallbacks();
+
+        this.scheduleOnce(() => {
+            this.bulletManager.put(this.node);
+        }, 0.1); // for better animation effect, I delay 0.1s when bullet hits the enemy
+    }
+}
---
+++ assets/Scripts/player.ts
@@ -0,0 +1,228 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class player extends cc.Component {
+
+    @property(cc.Prefab)
+    private bulletPrefab: cc.Prefab = null;
+
+    private moveDir: string = 'S';
+
+    private speed: number = 250;
+
+    private anim: cc.Animation = null;
+
+    private animateState = null;
+
+    private maxBullet: number = 5;
+
+    private bulletPool: cc.NodePool = null;
+
+    private nextAttackTime: number = 0;
+
+    private nextReloadTime: number = 0;
+
+    private attackCooldown: number = 0.2;
+
+    private reloadCooldown: number = 5;
+    
+    private attacking: boolean = false;
+
+    private enemys: cc.Node = null;
+
+    private enemyCount: number = 0;
+
+    private targetPosition: cc.Vec2 = cc.v2(0, 0);
+
+    private targetDirection: string = '';
+
+    private nextTraceTime: number = 0;
+
+    private traceCooldown: number = 0.5;
+
+
+    onLoad () {
+        this.bulletPool = new cc.NodePool('bullet');
+        this.anim = this.getComponent(cc.Animation);
+        this.enemys = cc.find("Canvas/enemy");
+        this.enemyCount = this.enemys.childrenCount;
+    }
+
+    start() {
+        for(let i: number = 0; i < this.maxBullet; i++) {
+            let bullet = cc.instantiate(this.bulletPrefab);
+            this.bulletPool.put(bullet);
+        }
+
+    }
+
+    playerMoveDir(dir: string) {
+        this.moveDir = dir;
+    }
+
+    playerMove(dt: number) {
+        switch (this.moveDir) {
+            case 'N':
+                this.node.y += this.speed * dt;
+                break;
+            case 'S':
+                this.node.y -= this.speed * dt;
+                break;
+            case 'E':
+                this.node.x += this.speed * dt;
+                break;
+            case 'W':
+                this.node.x -= this.speed * dt;
+                break;
+            case 'NE':
+                this.node.x += this.speed * dt;
+                this.node.y += this.speed * dt;
+                break;
+            case 'NW':
+                this.node.x -= this.speed * dt;
+                this.node.y += this.speed * dt;
+                break;
+            case 'SW':
+                this.node.x -= this.speed * dt;
+                this.node.y -= this.speed * dt;
+                break;
+            case 'SE':
+                this.node.x += this.speed * dt;
+                this.node.y -= this.speed * dt;
+                break;
+
+        }
+    }
+
+    playerWalkAnimation() {
+        if(this.attacking)
+            return; 
+            
+        switch (this.moveDir) {
+            case 'N':
+                if(this.animateState == null || this.animateState.name != 'playerWalkN')
+                    this.animateState = this.anim.play('playerWalkN');
+                break;
+            case 'S':
+                if(this.animateState == null || this.animateState.name != 'playerWalkS')
+                    this.animateState = this.anim.play('playerWalkS');
+                break;
+            case 'E':
+                if(this.animateState == null || this.animateState.name != 'playerWalkE')
+                    this.animateState = this.anim.play('playerWalkE');
+                break;
+            case 'W':
+                if(this.animateState == null || this.animateState.name != 'playerWalkW')
+                    this.animateState = this.anim.play('playerWalkW');
+                break;
+            case 'NE':
+                if(this.animateState == null || this.animateState.name != 'playerWalkNE')
+                    this.animateState = this.anim.play('playerWalkNE');
+                break;
+            case 'NW':
+                if(this.animateState == null || this.animateState.name != 'playerWalkNW')
+                    this.animateState = this.anim.play('playerWalkNW');
+                break;
+            case 'SW':
+                if(this.animateState == null || this.animateState.name != 'playerWalkSW')
+                    this.animateState = this.anim.play('playerWalkSW');
+                break;
+            case 'SE':
+                if(this.animateState == null || this.animateState.name != 'playerWalkSE')
+                    this.animateState = this.anim.play('playerWalkSE');
+                break;
+            default:
+                this.anim.stop();
+                break;
+        }
+    }
+
+    playerAttackAnimation() {
+        let angle = calcAngleDegrees(this.targetPosition.x - this.node.x, this.targetPosition.y - this.node.y);
+        if(angle < 157.5 && angle >= 112.5){
+            this.targetDirection = 'NW';
+        }
+        else if(angle < 112.5 && angle >= 67.5) {
+            this.targetDirection = 'N'
+        }
+        else if(angle < 67.5 && angle >= 22.5) {
+            this.targetDirection = 'NE';
+        }
+        else if(angle < 22.5 && angle >= -22.5) {
+            this.targetDirection = 'E';
+        }
+        else if(angle < -22.5 && angle >= -67.5){
+            this.targetDirection = 'SE';
+        }
+        else if(angle < -67.5 && angle >= -112.5){
+            this.targetDirection = 'S';
+        }
+        else if(angle < -112.5 && angle >= -157.5) {
+            this.targetDirection = 'SW';
+        }
+        else{
+            this.targetDirection = 'W';
+        }
+        this.anim.stop();
+        this.attacking = true;
+        this.animateState = this.anim.play('playerAttack' + this.targetDirection);
+        this.anim.on('finished', (e) =>{
+            this.attacking = false;
+        })
+    }
+
+    playerAttack() {
+        let currentTime = cc.director.getTotalTime() / 1000.0;
+        if(currentTime >= this.nextAttackTime){
+            this.nextAttackTime = currentTime + this.attackCooldown;
+            this.createBullet();
+            this.playerAttackAnimation();
+        }
+    }   
+
+    createBullet() {
+        let bullet = null;
+        if (this.bulletPool.size() > 0){
+            bullet = this.bulletPool.get(this.bulletPool);
+        }
+
+        if(bullet != null)
+            bullet.getComponent('bullet').init(this.node, this.targetPosition, this.targetDirection);
+    }
+    
+    traceEnemy() {
+        let currentTime = cc.director.getTotalTime() / 1000.0;
+        if(currentTime >= this.nextTraceTime){
+            this.nextTraceTime = currentTime + this.traceCooldown;
+            let nextTargetDistance = 9007199254740992; // INT_MAX
+            let nextTargetPosition = cc.v2(0, 0);
+            for(let i = 0; i < this.enemyCount; i++){
+                let currentDistance = this.node.position.sub(this.enemys.children[i].position).mag()
+                if(currentDistance < nextTargetDistance) {
+                    nextTargetDistance = currentDistance;
+                    nextTargetPosition = cc.v2(this.enemys.children[i].position.x, this.enemys.children[i].position.y);
+                }
+            }
+            this.targetPosition = nextTargetPosition;
+        }
+    }
+
+    update(dt) {
+        this.playerMove(dt);
+        this.playerWalkAnimation();
+        this.traceEnemy();
+    }
+}
+
+
+function calcAngleDegrees(x, y) {
+    return Math.atan2(y, x) * 180 / Math.PI;
+}
